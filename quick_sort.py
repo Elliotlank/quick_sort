1edf927a55264fbbea19b81e3dceb7b117a0a9aa
lk199 <lk199821@vip.qq.com>
2020-08-19 03:28:51 +1000

    first version

---
+++ quick_sort.py
@@ -0,0 +1,10 @@
+num_id = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(num_id)
+while True:
+    link_a = int(input("enter first numbers"))
+    link_b = int(input("enter second numbers"))
+    for i in range(len(num_id)):
+        if num_id[i] == num_id[link_a]:
+            num_id[i] = num_id[link_b]
+
+    print(num_id)
